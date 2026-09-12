b6931484da5cd7fefe8a13e87c6a2ca276d960e9
AnnaJever <65163970+AnnaJever@users.noreply.github.com>
2026-09-12 23:41:15 +0200

    Update SDM intro

---
+++ scripts/Simple_SDM_BetPen.R
@@ -0,0 +1,14 @@
+#### Species distribution model of Betula pendula in Germany ####
+
+# aim: build SDM for the tree species Betula penudla using a simple sdm
+
+# packages ----
+library(terra)
+
+# data ----
+
+# model ----
+
+# projection ----
+
+# save output ----
